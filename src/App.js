b10e3701d084106ebb931d7706d95ead42c73510
Jere <jirimicvl@outlook.com>
2023-01-05 14:25:47 +0200

    Counter app using recoil

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import Buttons from './components/Button';
 import {counter} from './atom/atom';
 
 // Material UI
-import { Container, Paper } from '@mui/material';
+import { Box, Card, Container, Divider, Grid, Paper, Stack, Typography } from '@mui/material';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
   const [count] = useRecoilState(counter);
@@ -16,17 +25,39 @@ function App() {
     <>
       <Header/>
       <Container>
-        <Paper>
-
-          <div>Count: {count} </div>
-          {Buttons(+1)}
-          {Buttons(+5)}
-          {Buttons(+10)}
-          {Buttons(0)}
-          {Buttons(-1)}
-          {Buttons(-5)}
-          {Buttons(-10)}
+        <Paper elevation={24} sx={{ m: 5, p: 5 }}>
+          <Box align="center" sx={{ height: 100,borderRadius: '50%' }}>
+            <Typography variant="h1">{count}</Typography>
+          </Box>
         </Paper>
+        <Grid container sx={{ justifyContent: 'space-evenly' }}>
+          <Grid>
+          <Typography>Increase</Typography>
+            <Stack
+              direction="column"
+              spacing={5}
+            >
+                {Buttons(-1)}
+                {Buttons(-5)}
+                {Buttons(-10)}
+            </Stack>
+          </Grid>
+          <Grid align="center">
+          <Typography>Reset</Typography>
+          {Buttons(0)}
+          </Grid>
+          <Grid>
+          <Typography>Decrease</Typography>
+            <Stack
+              direction="column"
+              spacing={5}
+            >
+                {Buttons(-1)}
+                {Buttons(-5)}
+                {Buttons(-10)}
+            </Stack>
+          </Grid>
+        </Grid>
       </Container>
       {/* <Footer/> */}
     </>
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,10 +3,11 @@ import {counter} from '../atom/atom';
 import {
     useRecoilState
   } from 'recoil';
+  import {Button as MuiButton} from '@mui/material';
 
 export default function Button(value) {
     const [count, setCount] = useRecoilState(counter);
   return (
-    <button onClick={()=> value === 0 ? setCount(0) : setCount(count+value)}>{value}</button>
+    <MuiButton variant="contained" onClick={()=> value === 0 ? setCount(0) : setCount(count+value)}>{value}</MuiButton>
   )
 }
